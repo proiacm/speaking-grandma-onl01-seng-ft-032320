ffcf70ef99d0f46b9a6a993d570bd8fe7a60cc71
CiaraMaria Proia <proiacm@gmail.com>
2020-03-24 03:27:36 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,3 @@
-# Write a speak_to_grandma method.
 def speak_to_grandma (phrase)
 
 if phrase == "I LOVE YOU GRANDMA!"
@@ -9,8 +8,5 @@ puts "NO, NOT SINCE 1938!"
 
 else phrase
   puts "HUH?! SPEAK UP, SONNY!"
-
-
-
 
 end
