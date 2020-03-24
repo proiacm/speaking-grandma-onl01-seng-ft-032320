3f59547e02191d343f42bf6e470fbcba4afba2bc
CiaraMaria Proia <proiacm@gmail.com>
2020-03-24 03:46:20 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@ def speak_to_grandma (phrase)
 if phrase == "I LOVE YOU GRANDMA!"
    "I LOVE YOU TOO PUMPKIN!"
   
-elsif phrase == upcase
+elsif phrase.upcase
    "NO, NOT SINCE 1938!"
    
 else phrase == "Hi Nana, how are you?"
