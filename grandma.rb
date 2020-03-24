67ed7fcb48be4563a9ce40d9f6f20d2f3ec7825f
CiaraMaria Proia <proiacm@gmail.com>
2020-03-24 03:39:55 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -6,7 +6,7 @@ if phrase == "I LOVE YOU GRANDMA!"
 elsif phrase.upcase
    "NO, NOT SINCE 1938!"
    
-elsif "Hi Nana, how are you?"
+elsif phrase.downcase
     "HUH?! SPEAK UP, SONNY!"
 
 else phrase
