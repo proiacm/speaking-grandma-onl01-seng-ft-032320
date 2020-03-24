2a8f53cfe777f551db2dd8a81ee3acceda482d51
CiaraMaria Proia <proiacm@gmail.com>
2020-03-24 03:27:20 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -8,10 +8,7 @@ elsif phrase.upcase
 puts "NO, NOT SINCE 1938!"
 
 else phrase
-  puts ""
-# Whatever you say to grandma, she should respond with
-# HUH?! SPEAK UP, SONNY!
-# unless you shout it (type in all capitals).
+  puts "HUH?! SPEAK UP, SONNY!"
 
 
 
